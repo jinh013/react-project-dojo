c47655b4d96e5d1b84a4252af8db69f31f3cdf38
Jean Marie <jmrandrianant@gmail.com>
2021-01-11 15:24:20 +0300

    outputting list in react

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,24 @@
+import { useState } from 'react';
+
 const Home = () => {
+    //let name = 'Jean';
+    const [name, setName] = useState('Jean');
+    const[age, setAge] = useState(29);
 
     const handleClick = () => {
-        console.log("hello, Jean"); 
+        setName('Monia');
+        setAge(26);
     }
 
-    const handleClickAgain = (name) => {
-        console.log("hello " + name);
-        
-    }
+
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <p> { name } is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() => handleClickAgain('jean')}>click me again</button>
         </div>
 
      ); 
-}
+    }
  
-export default Home ;
+export default Home;
